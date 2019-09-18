1b2599148144c6337a218ca036ff84784750efde
Tej Pochiraju <tej@jaaga.in>
2019-09-18 09:57:51 +0530

    minor cleanup

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { DbService } from '../services/db.service';
 import { ActivatedRoute } from '@angular/router';
-import { first, map } from "rxjs/operators";
+import { first } from "rxjs/operators";
 import {environment} from '../../environments/environment';
 
 @Component({
@@ -9,14 +9,14 @@ import {environment} from '../../environments/environment';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, AfterViewInit {
+export class EditComponent implements OnInit {
 
   video = {};
   frames = {};
   frameNumbers = [];
   currentOSTFrame = 0;
   objectKeys = Object.keys;
-  frameDimensions = [1920,1080];
+  frameDimensions = [1920,1080]; // Actual dimensions of the frame. Used in scaling.
 
   @ViewChild('ostCleanImage', {static:false}) ostCleanImage:ElementRef;
   constructor(
@@ -24,38 +24,34 @@ export class EditComponent implements OnInit, AfterViewInit {
     private route: ActivatedRoute
   ) { }
 
-  ngAfterViewInit() {
-    // console.log(this.ostCleanImage);
-  }
-
-  async ngOnInit() {
-    this.route.params.pipe(first()).subscribe(async params => {
+  ngOnInit() {
+    this.route.params.pipe(first()).subscribe(params => {
       if (params.id) {
-        await this.getFrames(params.id); 
-        // console.log(this.ostCleanImage);
+        this.getFrames(params.id); 
       }
     });
   }
 
   async getFrames(video_ref) {
     this.video = (await this.db.getVideoByRef(video_ref))['data'];
-        this.video['frames'].map(frame => {
-          this.frames[frame.number] = {
-            boxes: [],
-            cleanImage: `${environment.cmsEndpoint}${frame.clean_image}`,
-            finalImage: `${environment.cmsEndpoint}${frame.final_image}`
-          };
-          const boxes =  JSON.parse(frame.boxes);
-          boxes.map((coords,i) => {
-            this.frames[frame.number].boxes.push({
-              'coords': coords,
-              'text': `Box ${i+1}`
-            });
-          });
-          this.frameNumbers.push(frame.number);
+    this.video['frames'].map(frame => {
+      this.frames[frame.number] = {
+        boxes: [],
+        cleanImage: `${environment.cmsEndpoint}${frame.clean_image}`,
+        finalImage: `${environment.cmsEndpoint}${frame.final_image}`
+      };
+      const boxes = JSON.parse(frame.boxes);
+      boxes.map((coords, i) => {
+        this.frames[frame.number].boxes.push({
+          'coords': coords,
+          'text': `Box ${i+1}`
         });
-        this.frameNumbers.sort((a,b) => a-b);
+      });
+      this.frameNumbers.push(frame.number);
+    });
+    this.frameNumbers.sort((a, b) => a - b);
   }
+
   getCoords(boxNumber) {
     let top = 0;
     let left = 0;
@@ -64,7 +60,6 @@ export class EditComponent implements OnInit, AfterViewInit {
     if (this.ostCleanImage) {
       scaleX = this.ostCleanImage.nativeElement.offsetWidth/this.frameDimensions[0];    
       scaleY = this.ostCleanImage.nativeElement.offsetHeight/this.frameDimensions[1];
-      // console.log(scaleX,scaleY);
       left = this.frames[this.currentOSTFrame].boxes[boxNumber].coords[0]*scaleX; 
       top = this.frames[this.currentOSTFrame].boxes[boxNumber].coords[1]*scaleY;
     }
